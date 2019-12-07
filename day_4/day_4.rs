111ac1518203a98ae19e40a046bb10605da956bd
Matheus Santana <embs@cin.ufpe.br>
2019-12-07 17:32:07 -0300

    Day 4 2nd :star:

--- day_4/day_4.rs
+++ day_4/day_4.rs
@@ -1,39 +1,44 @@
+use std::collections::HashMap;
+
 fn main() {
     let mut satisfying_passwords_count = 0;
 
     'outer: for p in 284639..=748759 {
-    //'outer: for p in 284639..=300000 {
-        let password = p.to_string();
+    //'outer: for p in 666666..=748759 {
+        let mut password = p.to_string();
 
         // It is a six-digit number.
         if password.len() != 6 { continue; }
 
-        let mut previous_char = 'a';
-        let mut adjacent_same_digits = false;
+        let mut previous_char = password.remove(0);
+        let mut chars_count = HashMap::new();
+
+        chars_count.insert(previous_char, 1);
 
         'inner: for c in password.chars() {
-            if previous_char == 'a' {
-                previous_char = c;
-                continue;
-            }
-
             if c < previous_char {
                 //println!("skipping because {} is smaller than {}", c, previous_char);
                 continue 'outer;
             }
 
-            if c == previous_char {
-                adjacent_same_digits = true;
-                //println!("{} is the same as {}", c, previous_char);
+            if chars_count.contains_key(&c) {
+                chars_count.insert(c, chars_count[&c] + 1);
+            } else {
+                chars_count.insert(c, 1);
             }
 
             previous_char = c;
         }
 
-        if adjacent_same_digits {
-            satisfying_passwords_count = satisfying_passwords_count + 1;
-            //println!("{} conforms", password);
+        for (c, count) in chars_count {
+            if count == 2 {
+                satisfying_passwords_count = satisfying_passwords_count + 1;
+                //println!("{} conforms", password);
+                continue 'outer;
+            }
         }
+
+        //println!("{} does not conform", password);
     }
 
     println!("{}", satisfying_passwords_count);
